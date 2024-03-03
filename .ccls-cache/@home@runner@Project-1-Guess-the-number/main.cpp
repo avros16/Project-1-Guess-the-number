9117ff616eac19e58b1e0cad386f1e465cc90526
Ana Rosario <arosario.mrd@gmail.com>
2024-03-03 20:09:40 +0000

    end of first round of guessing

--- .ccls-cache/@home@runner@Project-1-Guess-the-number/main.cpp
+++ .ccls-cache/@home@runner@Project-1-Guess-the-number/main.cpp
@@ -28,9 +28,9 @@ using std::cin;
   
   // Generate 3 random numbers
   //int random_number = distribution(gen);
- // int random_number2 = distribution(gen);
-  //int random_number3 = distribution(gen);
-
+//int random_number2 = distribution(gen);
+//int random_number3 = distribution(gen);
+int guess{0};
 
   //variables
   std::string rules; 
@@ -45,12 +45,87 @@ cout << "***********************************************************************
   cin >> rules;
 
   if (rules == "Y" || rules == "y") { //covers both Y and y. proud of this :3
-    cout << " \n" << "*************************************************************************\n" << "\n"<< "⦿ We will give you 3 opportunities to guess the number correctly.\n" << "⦿ The number will range from 0 to 99.\n" <<"⦿ Every correct guess gets you 1 point.\n⦿ You need 2 points to win.\n⦿ The number will change after every round. \n⦿ Hints will be offered.\n \n \n⦿ Are you ready (Y/N)?";
+    cout << " \n" << "*************************************************************************\n" << "\n"<< "⦿ We will give you 3 opportunities to guess the number correctly.\n" << "⦿ The number will range from 0 to 99.\n" <<"⦿ Every correct guess gets you 1 point.\n⦿ You need 2 points to win.\n⦿ The number will change after every round. \n⦿ Hints will be offered.\n \n" ;
   } else if (rules == "N" || rules == "n") {
-  cout << "Too cool for rules... we like that.\n";
+  cout << "\n"<< "Too cool for rules... we like that.\n";
 }
   
+cout << "Are you ready (Y/N)? ";
+cin >> rules; 
 
+if (rules == "Y" || rules == "y") {
+  cout << "\n  Good luck!\n";
+
+// coding game with this section bc user answered yes that they were ready to play. game play depends on this answer.
+
+  int random_number = distribution(gen);
+  cout << "Guess the number: ";
+  cin >> guess; 
+
+  if (guess != random_number){
+    cout << "Nope, thats not it! Would you like a hint? (Y/N) ";
+    cin >> rules;
+        if (rules == "Y" || rules == "y") {
+              if (guess > random_number){
+                cout << "Hint: Your guess is too high...\n\n";
+              } else if (guess < random_number) {
+                cout << "Hint: Your guess is too low...\n\n";
+              }
+          
+        }
+        
+  } else if (guess == random_number) {
+    cout << "You win!";
+  }
+
+  cout << "Guess the number: ";
+  cin >> guess; 
+
+  if (guess != random_number){
+    cout << "Nope, thats not it! Would you like a hint? (Y/N) ";
+    cin >> rules;
+        if (rules == "Y" || rules == "y") {
+              if (guess > random_number){
+                cout << "Hint: Your guess is too high...\n\n";
+              } else if (guess < random_number) {
+                cout << "Hint: Your guess is too low...\n\n";
+              }
+
+        }
+
+  } else if (guess == random_number) {
+    cout << "You win!";
+  }
+
+
+  cout << "Last chance... Guess the number: ";
+  cin >> guess; 
+
+
+  if (guess != random_number){
+    cout << "Nope, thats not it! Would you like a hint? (Y/N) ";
+    cin >> rules;
+        if (rules == "Y" || rules == "y") {
+              if (guess > random_number){
+                cout << "Hint: Your guess is too high...\n\n";
+              } else if (guess < random_number) {
+                cout << "Hint: Your guess is too low...\n\n";
+              }
+
+        }
+
+  } else if (guess == random_number) {
+    cout << "You win!";
+  }
+
+
+
+
+
+  
+} else if (rules == "N" || rules == "n") {
+  cout << "Okay, bye!"; //no game play if user is not ready to play
+}
   
   
 }
