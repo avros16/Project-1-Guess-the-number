f8c1d39d3967385e62beb3ba955cb9da12b200e8
Ana Rosario <arosario.mrd@gmail.com>
2024-03-03 18:43:16 +0000

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <random> //testing idea of using 2 digit random number instead of 1. Must include this to 
+
 
 int main() {
-  std::cout << "Hello World!\n";
+
+using std::cout;
+using std::cin;
+
+// Seed the random number generator. 
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+
+  // Define the distribution for two-digit numbers
+  std::uniform_int_distribution<int> distribution(10, 99);
+
+  
+  // Generate a random number
+  int random_number = distribution(gen);
 }
